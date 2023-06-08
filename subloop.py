b176ff9a3b44bec6061b85c9553e46d8d71cf294
yurashkurenko <yurashkurenko@users.noreply.github.com>
2023-06-08 18:25:17 +0300

    Create subloop.py

---
+++ subloop.py
@@ -0,0 +1,30 @@
+from config import *
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Result from connect: {}".format(
+        mqtt.connack_string(rc)))
+    # Выполняем подписку на фильтр темы vehicles/vehiclepi01/tests
+    client.subscribe("vehicles/vehiclepi01/tests", qos=2)
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("I've subscribed with QoS: {}".format(
+    granted_qos[0]))
+
+def on_message(client, userdata, msg):
+    print("Message received. Topic: {}. Payload: {}".format(
+        msg.topic,
+        str(msg.payload)))
+
+if __name__ == "__main__":
+    client = mqtt.Client(protocol=mqtt.MQTTv311)
+    client.on_connect = on_connect
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+#    client.tls_set(ca_certs = ca_certificate,
+#        certfile=client_certificate,
+#        keyfile=client_key)
+    client.connect(host=mqtt_server_host,
+        port=mqtt_server_port,
+        keepalive=mqtt_keepalive)
+    client.loop_forever()
